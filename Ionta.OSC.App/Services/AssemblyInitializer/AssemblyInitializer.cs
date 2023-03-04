3b974ae50cc897bac0dafe2443c2756036aef64c
Ionta <ionov.tolya@yandex.ru>
2023-03-04 21:09:00 +0400

    fix osc-61

--- Ionta.OSC.App/Services/AssemblyInitializer/AssemblyInitializer.cs
+++ Ionta.OSC.App/Services/AssemblyInitializer/AssemblyInitializer.cs
@@ -27,19 +27,6 @@ namespace Ionta.OSC.App.Services.AssemblyInitializer
                 var data = packages.Assembly.Select(e => e.Data);
                 _assemblyStore.Load(data, packages.Id);
             }
-            //AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
-        }
-        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
-        {
-            AppDomain domain = (AppDomain)sender;
-            foreach (Assembly asm in domain.GetAssemblies())
-            {
-                if (asm.FullName == args.Name)
-                {
-                    return asm;
-                }
-            }
-            throw new ApplicationException($"Can't find assembly {args.Name}");
         }
     }
 }
