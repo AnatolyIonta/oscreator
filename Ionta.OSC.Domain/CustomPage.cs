d91dde022880fdba0ef174179f33d8d71130e76f
Anatoly <Ionta.t@mail.ru>
2023-03-25 06:21:31 +0400

    workspacePage

---
+++ Ionta.OSC.Domain/CustomPage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ionta.OSC.Domain
+{
+    public class CustomPage : BaseEntity
+    {
+        public string Html { get; set; }
+    }
+}
---
+++ Ionta.OSC.Domain/Workspace.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ionta.OSC.Domain
+{
+    public class Workspace
+    {
+        public List<CustomPage> Pages { get; set; }
+    }
+}
