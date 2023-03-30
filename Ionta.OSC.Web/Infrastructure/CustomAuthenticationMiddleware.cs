2e367039b07b66e46b6115efdf4e313406affd5d
Anatoly <Ionta.t@mail.ru>
2023-03-30 23:20:32 +0400

    [osc-104] модуль обращения к бэку

--- Ionta.OSC.Web/Infrastructure/CustomAuthenticationMiddleware.cs
+++ Ionta.OSC.Web/Infrastructure/CustomAuthenticationMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using Ionta.OSC.Core.CustomControllers.ControllerLoaderService;
+﻿using Ionta.OSC.App.Services;
+using Ionta.OSC.Core.CustomControllers.ControllerLoaderService;
 using Ionta.OSC.ToolKit.Auth;
 
 using Microsoft.Net.Http.Headers;
@@ -10,14 +11,27 @@ namespace Ionta.OSC.Web.Infrastructure
         private readonly RequestDelegate _next;
         private readonly IControllerLoaderService _controllerLoader;
         private readonly IAuthenticationService _authentication;
-        public CustomAuthenticationMiddleware(RequestDelegate next, IControllerLoaderService controllerLoader, IAuthenticationService authentication) { 
+        private readonly IServiceProvider _serviceProvider;
+        public CustomAuthenticationMiddleware(RequestDelegate next, IControllerLoaderService controllerLoader, IAuthenticationService authentication, IServiceProvider serviceProvider) { 
             _next = next;
             _controllerLoader = controllerLoader;
             _authentication = authentication;
+            _serviceProvider = serviceProvider;
         }
         public async Task InvokeAsync(HttpContext context)
         {
             var controller = await _controllerLoader.GetControllerInfoFromPath(context.Request.Path);
+
+            using (var scoped = _serviceProvider.CreateScope())
+            {
+                var _userProvider = scoped.ServiceProvider.GetRequiredService<IUserProvider>();
+                if (_userProvider.GetCurrentId() == null && controller.InternalAuthorize)
+                {
+                    context.Response.StatusCode = 401;
+                    return;
+                }
+            }
+            
             if (controller != null && controller.Authorize)
             {
                 var accessToken = context.Request.Headers[HeaderNames.Authorization];
